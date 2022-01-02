1505de80b341d51f09c04f4f52ddfd110d9291f0
heetae <heetae@heetaeui-MacBookAir.local>
2022-01-03 02:24:34 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar(
+          title: GestureDetector(
+            onTap: (){
+              print('실행');
+            },
+            child: Row(
+              children: [
+                Text('개봉동'),
+                Icon(Icons.keyboard_arrow_down)
+              ],
+            ),
+          ),
+
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.search),
+                Icon(Icons.menu),
+                Icon(Icons.add_alert),
+              ],
+            )],
+        ),
+
+        body: SizedBox(
+            child: ElevatedButton(
+                child: Text('버튼'),
+                onPressed: (){},
+            )
+        ),
+
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children:[
+                  Icon(Icons.phone),
+                  Icon(Icons.message),
+                  Icon(Icons.contact_page),
+                ],)
+          ),
+        )
+
+
+
+      )
+
+    );
+  }
+}
